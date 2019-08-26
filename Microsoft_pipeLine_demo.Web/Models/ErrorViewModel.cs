2610ac689bd78ace6776b74771f122004f8fd52f
govindSingh <gchaudhary39@gmail.com>
2019-08-26 11:20:36 +0530

    Add project files.

---
+++ Microsoft_pipeLine_demo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Microsoft_pipeLine_demo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
